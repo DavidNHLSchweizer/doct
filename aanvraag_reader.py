2f957c26389c9fbf71493b21ec1001981a702a5f
David Schweizer <david.schweizer@nhlstenden.com>
2022-12-01 12:00:31 +0100

    initial

---
+++ aanvraag_reader.py
@@ -0,0 +1,89 @@
+import re
+from attr import dataclass
+from pathlib import Path
+import pandas
+import tabula
+
+@dataclass
+class AanvraagInfo:
+    datum: str = ''
+    student: str = ''
+    studnr: str = ''
+    telno: str = ''
+    email: str = ''
+    bedrijf: str = ''
+    titel: str = ''
+    def __str__(self):
+        return f'{self.student}({self.studnr}): {self.bedrijf} - "{self.titel}"'
+def nrows(table: pandas.DataFrame)->int:
+    return table.shape[0]
+
+class AanvraagReaderFromPDF:
+    def __init__(self, pdf_file: str):
+        self.aanvraag = AanvraagInfo()
+        self.read_pdf(pdf_file)
+    def read_pdf(self, pdf_file: str):
+        tables = tabula.read_pdf(pdf_file,pages='all')
+        self._parse_first_table(tables[0])
+        self._parse_title(tables[2])
+    def __convert_fields(self, fields_dict, translation_table):
+        for field in translation_table:            
+            setattr(self.aanvraag, translation_table[field], fields_dict.get(field, 'NOT FOUND'))
+    def __parse_table(self, table: pandas.DataFrame, start_row, end_row, translation_table):
+        table_dict = {}
+        if start_row >= nrows(table) or end_row >= nrows(table):
+            print(f'Error in parse_table ({start_row}, {end_row}): table has {nrows(table)} rows.')
+            return
+        for row in range(start_row, end_row):
+            table_dict[table.values[row][0]] = table.values[row][1]
+        self.__convert_fields(table_dict, translation_table)
+    def __rectify_table(self, table, row0, row1):
+        #necessary because some students somehow introduce \r characters in the table first column
+        for row in range(row0, row1):
+            table.values[row][0] = table.values[row][0].replace('\r', '')
+    def _parse_first_table(self, table: pandas.DataFrame):        
+        student_dict_fields = {'Datum/revisie': 'datum', 'Student': 'student', 'Studentnummer': 'studnr', 'Telefoonnummer': 'telno', 'E-mailadres': 'email', 'Bedrijfsnaam': 'bedrijf'}
+        student_dict_len  = len(student_dict_fields) + 5 # een beetje langer ivm bedrijfsnaam
+        self.__rectify_table(table, 0, student_dict_len)
+        self.__parse_table(table, 0, student_dict_len, student_dict_fields)        
+    def _parse_title(self, table: pandas.DataFrame)->str:
+        #regex because some students somehow lose the '.' characters
+        start_paragraph  = '4.*\(Voorlopige, maar beschrijvende\) Titel van de afstudeeropdracht'
+        end_paragraph = '5.*Wat is de aanleiding voor de opdracht\?'         
+        self.aanvraag.titel = ' '.join(self.__get_strings_from_table(table, start_paragraph, end_paragraph))
+    def __get_strings_from_table(self, table, start_paragraph_regex, end_paragraph_regex)->list[str]:
+        def row_matches(table, row, pattern:re.Pattern):
+            if isinstance(table.values[row][0], str):
+                return pattern.match(table.values[row][0]) is not None
+            else:
+                return False
+        result = []
+        row = 0
+        start_pattern = re.compile(start_paragraph_regex)
+        while row < nrows(table) and not row_matches(table, row, start_pattern):
+            row+=1
+        row+=1
+        end_pattern = re.compile(end_paragraph_regex)
+        while row < nrows(table) and not row_matches(table, row, end_pattern):
+            result.append(table.values[row][0])
+            row+=1
+        return result
+
+class AanvraagDirectory:
+    def __init__(self, directory):
+        self.directory = directory
+        self.aanvragen: list[AanvraagInfo] = []
+        self.__read_files(directory)
+    def __read_files(self, directory):
+        for file in Path(directory).glob('*.pdf'):
+            print(file)
+            try:           
+                self.aanvragen.append(aanvraag:=AanvraagReaderFromPDF(file).aanvraag)
+                print(aanvraag)
+            except Exception as E:
+                print(f'kan bestand {file}  niet lezen: {E}')
+
+if __name__=='__main__':
+    AD = AanvraagDirectory(r'C:\repos\doct\data')
+    for aanvraag in AD.aanvragen:
+        print(aanvraag)
