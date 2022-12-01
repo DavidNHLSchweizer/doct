51fc4dc33ae820db7acea10a168e9b04b0ba4db4
David Schweizer <david.schweizer@nhlstenden.com>
2022-12-01 12:28:31 +0100

    added sorting

--- aanvraag_reader.py
+++ aanvraag_reader.py
@@ -1,9 +1,10 @@
 import re
 from attr import dataclass
 from pathlib import Path
-import pandas
+import pandas as pd
 import tabula
 
+ERRCOMMENT = 'Waarschijnlijk niet een aanvraagformulier'
 class PDFReaderException(Exception): pass
 
 @dataclass
@@ -18,7 +19,7 @@ class AanvraagInfo:
     def __str__(self):
         return f'{self.student}({self.studnr}) - {self.datum}: {self.bedrijf} - "{self.titel}"'
 
-def nrows(table: pandas.DataFrame)->int:
+def nrows(table: pd.DataFrame)->int:
     return table.shape[0]
 
 class AanvraagReaderFromPDF:
@@ -32,10 +33,10 @@ class AanvraagReaderFromPDF:
     def __convert_fields(self, fields_dict, translation_table):
         for field in translation_table:            
             setattr(self.aanvraag, translation_table[field], fields_dict.get(field, 'NOT FOUND'))
-    def __parse_table(self, table: pandas.DataFrame, start_row, end_row, translation_table):
+    def __parse_table(self, table: pd.DataFrame, start_row, end_row, translation_table):
         table_dict = {}
         if start_row >= nrows(table) or end_row >= nrows(table):
-            raise PDFReaderException(f'Fout in parse_table ({start_row}, {end_row}): de tabel heeft {nrows(table)} rijen.\nWaarschijnlijk niet het aanvraagformulier maar een ander soort PDF.')
+            raise PDFReaderException(f'Fout in parse_table ({start_row}, {end_row}): de tabel heeft {nrows(table)} rijen.\n{ERRCOMMENT}.')
         for row in range(start_row, end_row):
             table_dict[table.values[row][0]] = table.values[row][1]
         self.__convert_fields(table_dict, translation_table)
@@ -44,17 +45,17 @@ class AanvraagReaderFromPDF:
         for row in range(row0, row1):
             if isinstance(table.values[row][0], str): #sometimes there is an empty cell that is parsed by tabula as a float NAN
                 table.values[row][0] = table.values[row][0].replace('\r', '')
-    def _parse_main_data(self, table: pandas.DataFrame):        
+    def _parse_main_data(self, table: pd.DataFrame):        
         student_dict_fields = {'Datum/revisie': 'datum', 'Student': 'student', 'Studentnummer': 'studnr', 'Telefoonnummer': 'telno', 'E-mailadres': 'email', 'Bedrijfsnaam': 'bedrijf'}
         student_dict_len  = len(student_dict_fields) + 5 # een beetje langer ivm bedrijfsnaam
         self.__rectify_table(table, 0, student_dict_len)
         self.__parse_table(table, 0, student_dict_len, student_dict_fields)        
-    def _parse_title(self, table: pandas.DataFrame)->str:
+    def _parse_title(self, table: pd.DataFrame)->str:
         #regex because some students somehow lose the '.' characters or renumber the paragraphs
         start_paragraph  = '\d.*\(Voorlopige, maar beschrijvende\) Titel van de afstudeeropdracht'
         end_paragraph = '\d.*Wat is de aanleiding voor de opdracht\?'         
         self.aanvraag.titel = ' '.join(self.__get_strings_from_table(table, start_paragraph, end_paragraph))
-    def __get_strings_from_table(self, table, start_paragraph_regex, end_paragraph_regex)->list[str]:
+    def __get_strings_from_table(self, table:pd.DataFrame, start_paragraph_regex:str, end_paragraph_regex:str)->list[str]:
         def row_matches(table, row, pattern:re.Pattern):
             if isinstance(table.values[row][0], str):
                 return pattern.match(table.values[row][0]) is not None
@@ -77,6 +78,7 @@ class AanvraagDirectory:
         self.directory = directory
         self.aanvragen: list[AanvraagInfo] = []
         self.__read_files(directory)
+        self.__sort_files()
     def __read_files(self, directory):
         for file in Path(directory).glob('*.pdf'):
             print(file)
@@ -84,7 +86,9 @@ class AanvraagDirectory:
                 self.aanvragen.append(aanvraag:=AanvraagReaderFromPDF(file).aanvraag)
                 print(aanvraag)
             except Exception as E:
-                print(f'kan bestand {file}  niet lezen: {E}')
+                print(f'***ERROR***: Kan bestand {file}  niet lezen: {E}\n{ERRCOMMENT}.')
+    def __sort_files(self):
+        self.aanvragen.sort(key=lambda aanvraag: (aanvraag.student, aanvraag.datum))
 
 if __name__=='__main__':
     AD = AanvraagDirectory(r'C:\repos\doct\data')
