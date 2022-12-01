a51a297304a2745d19198dc0205afdb43b1df9de
David Schweizer <david.schweizer@nhlstenden.com>
2022-12-01 14:14:37 +0100

    almost ready. this can be merged

--- mm1.py
+++ mm1.py
@@ -3,14 +3,20 @@ from mailmerge import MailMerge
 
 template = "template 0.6.docx"
 
-data = pd.read_excel('aanvragen.xlsx')
-print(data)
+# data = pd.read_excel('aanvragen.xlsx')
+# print(data)
 
 def nrows(table):
     return table.shape[0]
 
-for row in range(1,nrows(data)):
-    student,bedrijf,titel,datumversie=data.values[row][0],data.values[row][5],data.values[row][6],data.values[row][4]
-    document = MailMerge(template)
-    document.merge(student=student,bedrijf=bedrijf,titel=titel,datumversie=datumversie)
-    document.write(f'aanvraag {student}.docx')
+class DocumentCreator:
+    def __init__(self, data: pd.DataFrame, template: str, output_prefix: str):
+        for row in range(1,nrows(data)):
+            student,bedrijf,titel,datumversie=data.values[row][0],data.values[row][5],data.values[row][6],data.values[row][4]
+            document = MailMerge(template)
+            document.merge(student=student,bedrijf=bedrijf,titel=titel,datumversie=datumversie)
+            output_name = f'{output_prefix} {student}.docx'
+            document.write(output_name)
+            print(f'{student} written to {output_name}')
+
+DocumentCreator(pd.read_excel('aanvragen.xlsx'), template, r'beoordelingen\Beoordeling')
